7de73c5c230f4f04af90bc1be047479f1a8e93d0
RKs <softwarekashyap@gmail.com>
2020-06-30 14:54:48 +0530

    Product Next Prev On Details page

---
+++ Block/Nextpre.php
@@ -0,0 +1,118 @@
+<?php
+
+/**
+ * @author Kashyap Team
+ * @copyright Copyright (c) 2018 Kashyap (softwarekashyap@gmail.com)
+ * @package Kashyap_Nextpre
+*/
+
+namespace Kashyap\Nextpre\Block;
+
+class Nextpre extends \Magento\Framework\View\Element\Template
+{
+    protected $_objectManager;
+    
+    /**
+     * @var Product
+     */
+    protected $_product = null;
+
+    /**
+     * Core registry
+     *
+     * @var \Magento\Framework\Registry
+     */
+    protected $_coreRegistry = null;
+
+    private $scopeConfig;
+
+    /**
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param \Magento\Framework\Registry $registry
+     * @param \Magento\Framework\ObjectManagerInterface $objectManager,
+     * @param array $data
+     */
+
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Framework\ObjectManagerInterface $objectManager,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        array $data = []
+    ) {
+        $this->_coreRegistry = $registry;
+        $this->_objectManager = $objectManager;
+        $this->scopeConfig = $scopeConfig;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * @return Product
+     */
+    public function getProduct()
+    {
+        if (!$this->_product) {
+            $this->_product = $this->_coreRegistry->registry('product');
+        }
+        return $this->_product;
+    }
+
+    public function getModel($model) {
+        return $this->_objectManager->create($model);
+    }
+
+    public function getCategoryProductIds($current_category) {
+        $category_products = $current_category->getProductCollection()
+            ->addAttributeToSelect('*')
+            ->addAttributeToFilter('status', array('eq' => 1))
+            ->addAttributeToFilter('visibility', array('eq' => 4))
+            ->addAttributeToSort('position', 'asc');
+            
+        $cat_prod_ids = $category_products->getAllIds();
+        
+        return $cat_prod_ids;
+    }
+
+
+    public function getPrevProduct($product) {
+        $current_category = $product->getCategory();
+        if(!$current_category) {
+            foreach($product->getCategoryCollection() as $parent_cat) {
+                $current_category = $parent_cat;
+            }
+        }
+        if(!$current_category)
+            return false;
+        $cat_prod_ids = $this->getCategoryProductIds($current_category);
+        $_pos = array_search($product->getId(), $cat_prod_ids);
+        if (isset($cat_prod_ids[$_pos - 1])) {
+            $prev_product = $this->getModel('Magento\Catalog\Model\Product')->load($cat_prod_ids[$_pos - 1]);
+            return $prev_product;
+        }
+        return false;
+    }
+    
+    public function getNextProduct($product) {
+        $current_category = $product->getCategory();
+        if(!$current_category) {
+            foreach($product->getCategoryCollection() as $parent_cat) {
+                $current_category = $parent_cat;
+            }
+        }
+        if(!$current_category)
+            return false;
+        $cat_prod_ids = $this->getCategoryProductIds($current_category);
+        $_pos = array_search($product->getId(), $cat_prod_ids);
+        if (isset($cat_prod_ids[$_pos + 1])) {
+            $next_product = $this->getModel('Magento\Catalog\Model\Product')->load($cat_prod_ids[$_pos + 1]);
+            return $next_product;
+        }
+        return false;
+    }
+
+    public function getConfigure($value = 'active')
+    {
+        return $this->scopeConfig->getValue('nextpre/general/'.$value, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+    }
+
+}
